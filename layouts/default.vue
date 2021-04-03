c9b89acd7406d65c1844246f97e3c6ed44565f2e
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-04-03 15:58:06 -0600

    chore: 🎨 reorganize layout components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,21 @@
 <template>
   <div>
-    <header>
-      <navbar />
-    </header>
+    <Header />
     <Nuxt />
+    <Footer />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-import Navbar from '@/components/navigation/Navbar.vue'
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 
 export default Vue.extend({
   components: {
-    Navbar
+    Header,
+    Footer
   }
 })
 </script>
