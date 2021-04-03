0627c813f59cf374ef04ab2edd7f09edeebe7b52
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-04-03 16:22:25 -0600

    feat: ✨ add the travels repository

---
+++ repositories/TravelRepository.ts
@@ -0,0 +1,15 @@
+import api from '@/lib/api'
+
+import TravelParamsInterface from '@/types/TravelParamsInterface'
+
+/**
+ * Fetch all the travels
+ *
+ * @param {TravelParamsInterface} params Request parameters.
+ * @return {Promise}
+ */
+export const all = (params?: TravelParamsInterface): Promise<any> => {
+  return api.get('/travels', {
+    params
+  })
+}
