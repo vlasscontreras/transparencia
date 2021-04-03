c6ccf122cad2da18bf9a498e2dd50741584baf4e
Vlass Contreras <1564743-vlasscontreras@users.noreply.gitlab.com>
2021-04-03 13:44:39 -0600

    feat: ✨ add the navbar menu

---
+++ components/navigation/NavMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li>
+      <nuxt-link to="/viajes" class="block py-3 md:py-0">
+        Viajes
+      </nuxt-link>
+    </li>
+    <li>
+      <nuxt-link to="/obras" class="block py-3 md:py-0">
+        Obras
+      </nuxt-link>
+    </li>
+    <li>
+      <nuxt-link to="/servicios" class="block py-3 md:py-0">
+        Servicios
+      </nuxt-link>
+    </li>
+    <li>
+      <nuxt-link to="/documentos" class="block py-3 md:py-0">
+        Documentos
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
