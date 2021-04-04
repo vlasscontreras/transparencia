7688a6bb8579db99e55a6ccaf027e15d10d2fb1b
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-04-04 14:40:05 -0600

    feat: ✨ add the Breadcrumbs component

---
+++ components/navigation/Breadcrumbs.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="flex items-center text-sm font-medium">
+    <nuxt-link to="/" class="text-gray-500 hover:text-gray-700 focus:outline-none focus:underline transition duration-150 ease-in-out">
+      Inicio
+    </nuxt-link>
+
+    <Chevron v-if="to && title" class="flex-shrink-0 mx-2 h-5 w-5 text-gray-400" />
+
+    <nuxt-link v-if="to && title" :to="to" class="text-gray-500 hover:text-gray-700 focus:outline-none focus:underline transition duration-150 ease-in-out">
+      {{ title }}
+    </nuxt-link>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import Chevron from '@/components/icons/Chevron.vue'
+
+export default Vue.extend({
+  components: {
+    Chevron
+  },
+
+  props: {
+    to: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
