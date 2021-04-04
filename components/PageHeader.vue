ccd133bd634cf4204478015582b6c09ac20d89d7
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-04-04 14:40:27 -0600

    feat: ✨ add the Page Header component

---
+++ components/PageHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="hidden sm:block pb-1">
+      <Breadcrumbs :to="to" :title="title" />
+    </div>
+    <div class="sm:flex sm:justify-between">
+      <h2 class="text-2xl font-semibold font-sans text-gray-900 sm:text-xl">
+        {{ title }}
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import Breadcrumbs from '~/components/navigation/Breadcrumbs.vue'
+
+export default Vue.extend({
+  components: {
+    Breadcrumbs
+  },
+
+  props: {
+    to: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
