75d164b622382d06a7a767970d80da0b0479379c
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-04-06 21:00:17 -0600

    feat: ✨create an API proxy

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/https://www.transparencia.gob.sv/api/v1'
+  baseURL: '/api/v1'
 })
 
 api.defaults.headers.common['Content-Type'] = 'application/json'
