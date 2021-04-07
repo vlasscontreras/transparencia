df483f3b542fa255b0f3c41eeae5650fd3731e06
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-04-06 20:44:06 -0600

    chore: 🎨 add pagination and move fetch logic outside collection

--- components/travels/TravelCollection.vue
+++ components/travels/TravelCollection.vue
@@ -1,18 +1,5 @@
 <template>
-  <!-- Loading -->
-  <div v-if="travels === null">
-    <div v-for="n in 6" :key="`loader-${n}`" class="shadow-lg rounded-xl p-4 bg-white">
-      <TravelLoader />
-    </div>
-  </div>
-
-  <!-- Error -->
-  <div v-else-if="travels === false">
-    <GenericRetry class="mt-3 sm:mt-0 col-span-3 px-4" @retry="fetchTravels" />
-  </div>
-
-  <!-- Data -->
-  <div v-else-if="travels.length > 0">
+  <div>
     <TravelItem v-for="travel in travels" :key="`travel-${travel.id}`" :data="travel" />
   </div>
 </template>
@@ -20,37 +7,18 @@
 <script lang="ts">
 import Vue from 'vue'
 
-import * as TravelRepository from '@/repositories/TravelRepository'
 import TravelItem from '@/components/travels/TravelItem.vue'
-import TravelLoader from '@/components/loaders/TravelLoader.vue'
-import GenericRetry from '@/components/errors/GenericRetry.vue'
+import Travel from '@/types/Travel'
 
 export default Vue.extend({
   components: {
-    TravelItem,
-    TravelLoader,
-    GenericRetry
+    TravelItem
   },
 
-  data: () => ({
-    travels: null as Boolean | null
-  }),
-
-  mounted () {
-    this.fetchTravels()
-  },
-
-  methods: {
-    fetchTravels () {
-      this.travels = null
-
-      TravelRepository.all()
-        .then((response) => {
-          this.travels = response.data
-        })
-        .catch(() => {
-          this.travels = false
-        })
+  props: {
+    travels: {
+      type: Array as () => Array<Travel>,
+      default: null
     }
   }
 })
--- pages/travels.vue
+++ pages/travels.vue
@@ -2,7 +2,23 @@
   <div class="container mx-auto py-10 md:pb-20 px-4">
     <PageHeader title="Viajes" to="/travels" class="mb-7" />
 
-    <TravelCollection class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 -mx-4 sm:mx-0" />
+    <!-- Loading -->
+    <div v-if="travels === null" :class="gridClass">
+      <div v-for="n in 6" :key="`loader-${n}`" class="shadow-lg rounded-xl p-4 bg-white">
+        <TravelLoader />
+      </div>
+    </div>
+
+    <!-- Error -->
+    <div v-else-if="travels === false">
+      <GenericRetry class="mt-3 sm:mt-0 px-4" @retry="fetchTravels" />
+    </div>
+
+    <!-- Data -->
+    <TravelCollection v-else-if="travels.length > 0" :class="gridClass" :travels="travels" />
+
+    <!-- Pagination -->
+    <Pagination v-if="travels && travels.length > 0" :current="currentPage" class="mt-10" @navigate="goTo" />
   </div>
 </template>
 
@@ -10,16 +26,71 @@
 import Vue from 'vue'
 
 import { title } from '@/lib/meta'
+import * as TravelRepository from '@/repositories/TravelRepository'
 import TravelCollection from '@/components/travels/TravelCollection.vue'
-import PageHeader from '~/components/PageHeader.vue'
+import PageHeader from '@/components/PageHeader.vue'
+import TravelLoader from '@/components/loaders/TravelLoader.vue'
+import GenericRetry from '@/components/errors/GenericRetry.vue'
+import Pagination from '@/components/navigation/Pagination.vue'
 
 export default Vue.extend({
   components: {
     TravelCollection,
-    PageHeader
+    PageHeader,
+    TravelLoader,
+    GenericRetry,
+    Pagination
   },
+
+  data: () => ({
+    travels: null as Boolean | null,
+    currentPage: 1,
+    gridClass: [
+      'grid',
+      'grid-cols-1',
+      'sm:grid-cols-2',
+      'lg:grid-cols-3',
+      'gap-10',
+      '-mx-4',
+      'sm:mx-0'
+    ]
+  }),
+
   head: {
     title: title('Viajes')
+  },
+
+  watch: {
+    currentPage (newValue) {
+      this.fetchTravels(newValue)
+    }
+  },
+
+  mounted () {
+    this.fetchTravels()
+  },
+
+  methods: {
+    goTo (page: number) {
+      this.currentPage = page
+    },
+
+    fetchTravels (page: number = 1, limit: number = 9) {
+      this.travels = null
+
+      const params = {
+        page,
+        per_page: limit
+      }
+
+      TravelRepository.all(params)
+        .then((response) => {
+          this.travels = response.data
+        })
+        .catch(() => {
+          this.travels = false
+        })
+    }
   }
 })
 </script>
