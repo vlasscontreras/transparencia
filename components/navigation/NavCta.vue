5229efb4fcd5f05676f7e8341b56bd060d49b4f3
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-04-08 19:43:45 -0600

    fix: 💄🐛 GitHub icon height on Safari

--- components/navigation/NavCta.vue
+++ components/navigation/NavCta.vue
@@ -3,8 +3,8 @@
     <a href="https://www.transparencia.gob.sv/api/v1/home" target="_blank" rel="noopener noreferrer" class="inline-flex">
       Fuente
     </a>
-    <a href="https://github.com/vlasscontreras/transparencia" target="_blank" rel="noopener noreferrer" class="flex space-x-2 rounded border px-2 py-1 hover:opacity-80 transition-opacity">
-      <GitHub class="w-4" />
+    <a href="https://github.com/vlasscontreras/transparencia" target="_blank" rel="noopener noreferrer" class="flex items-center space-x-2 rounded border px-2 py-1 hover:opacity-80 transition-opacity">
+      <GitHub class="w-4 h-4" />
       <span>Código</span>
     </a>
   </div>
