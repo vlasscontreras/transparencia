86f56bb30294f05ef6f4280a45064d67dcb03c01
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-04-17 17:19:23 -0600

    chore: 🎨 remove unnecessary `div` wrapping dropdown button

--- components/navigation/Dropdown.vue
+++ components/navigation/Dropdown.vue
@@ -1,18 +1,16 @@
 <template>
   <div v-click-outside="toggle" class="relative inline-block text-left">
-    <div>
-      <button
-        id="options-menu"
-        type="button"
-        class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
-        aria-expanded="true"
-        aria-haspopup="true"
-        @click="toggle"
-      >
-        {{ current }}
-        <Chevron class="h-5 w-5 transform rotate-90 ml-2" />
-      </button>
-    </div>
+    <button
+      id="options-menu"
+      type="button"
+      class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+      aria-expanded="true"
+      aria-haspopup="true"
+      @click="toggle"
+    >
+      {{ current }}
+      <Chevron class="h-5 w-5 transform rotate-90 ml-2" />
+    </button>
 
     <transition name="slide-fade">
       <div v-if="expanded" class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
